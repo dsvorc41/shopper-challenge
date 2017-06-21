f4027e460ddf4e763df045a2f2e3bb8cc0492c33
Dan Svorcan <danilosvorcan@live.com>
2017-06-20 22:24:16 -0700

    Add react router and start dashboard page

--- shopper-challenge/src/index.js
+++ shopper-challenge/src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import ReactDOM from 'react-dom';
 import App from './App';
+import Dashboard from './components/Dashboard';
+
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+  <BrowserRouter>
+    <Switch>
+      <Route exact path='/' component={App}/>
+      <Route exact path='/dashboard' component={Dashboard}/>
+    </Switch>
+  </BrowserRouter>
+  ), document.getElementById('root'));
 registerServiceWorker();
